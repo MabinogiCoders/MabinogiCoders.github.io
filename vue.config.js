3720ed047fd8e3611543e31e68652c65724e4c47
jsmin6330 <jsmin6330@gmail.com>
2024-09-27 10:13:04 +0900

    Configure publicPath for GitHub Pages deployment and build project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/MabinogiCoders.github.io/'  // GitHub Pages에서 사용하는 경로 (레포지토리 이름과 일치)
+    : '/'
 })
